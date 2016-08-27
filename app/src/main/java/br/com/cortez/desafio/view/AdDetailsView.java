2799d04ff55d0c8a0b652ff7afde71530511a365
Pedro Cortez <pedro.fellipe.cortez@gmail.com>
2016-08-27 14:47:36 -0300

    - Put data   on view  ad_detail - create view ad detail - create style text

---
+++ app/src/main/java/br/com/cortez/desafio/view/AdDetailsView.java
@@ -0,0 +1,6 @@
+package br.com.cortez.desafio.view;
+
+public interface AdDetailsView {
+
+    void showAdDetail();
+}
--- app/src/main/java/br/com/cortez/desafio/view/activity/AdDetailsAct.java
+++ app/src/main/java/br/com/cortez/desafio/view/activity/AdDetailsAct.java
@@ -4,17 +4,46 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import br.com.cortez.desafio.ChallengeApplication;
 import br.com.cortez.desafio.R;
+import br.com.cortez.desafio.imageLoader.ImageLoader;
 import br.com.cortez.desafio.model.Ad;
+import br.com.cortez.desafio.view.AdDetailsView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
-public class AdDetailsAct extends AppCompatActivity {
+public class AdDetailsAct extends AppCompatActivity implements AdDetailsView{
+
+
+    private static final String EXTRA_AD = "ad";
+    private Ad ad;
+    private ImageLoader imageLoader;
+
+    @BindView(R.id.olx_ad_photo)
+    ImageView detail;
+    @BindView(R.id.olx_ad_title)
+    TextView title;
+    @BindView(R.id.olx_ad_price)
+    TextView price;
+    @BindView(R.id.olx_ad_date)
+    TextView date;
+    @BindView(R.id.olx_ad_seller)
+    TextView seller;
+    @BindView(R.id.olx_ad_zipcode)
+    TextView zipcode;
+    @BindView(R.id.olx_ad_detail_toolbar)
+    Toolbar toolbar;
+
 
 
     public static void start(Activity activity, Ad ad) {
         Intent i = new Intent(activity, AdDetailsAct.class);
         Bundle bundle = new Bundle();
-        bundle.putSerializable("Ad", ad);
+        bundle.putSerializable(EXTRA_AD, ad);
         i.putExtras(bundle);
         activity.startActivity(i);
     }
@@ -23,6 +52,30 @@ public class AdDetailsAct extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ad_details);
+
+        ButterKnife.bind(this);
+
+        Bundle extras = getIntent().getExtras();
+        ad = (Ad) extras.getSerializable(EXTRA_AD);
+
+        imageLoader = ChallengeApplication.getInstance().provideImageLoader();
+
+        showAdDetail();
+
     }
 
+
+    @Override
+    public void showAdDetail() {
+
+        setSupportActionBar(toolbar);
+
+        imageLoader.load(ad.getPhotos().getDetailHd(), detail);
+        title.setText(ad.getTitle());
+        price.setText(ad.getFormattedPrice());
+        date.setText(ad.getDate());
+        seller.setText(ad.getSeller());
+        zipcode.setText(ad.getZipcode());
+
+    }
 }
